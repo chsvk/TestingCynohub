ffe49599b81321f4f448d9fdbdaf1c563bb92334
Vamshi Krishna <chsvk21@gmail.com>
2022-05-07 11:38:00 +0530

    Base PDF Viewer with PDFJS-Dist

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,29 +1,54 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+         <div id="pageContainer">
+            <div id="viewer" class="pdfViewer"></div>     
+          </div>
     </ion-content>
   </ion-page>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+<script>
 
-export default  defineComponent({
+import { IonPage, IonContent } from '@ionic/vue';
+import pdfjsLib from "pdfjs-dist/build/pdf";
+import { PDFViewer } from "pdfjs-dist/web/pdf_viewer";
+import "pdfjs-dist/web/pdf_viewer.css";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdn.jsdelivr.net/npm/pdfjs-dist@2.0.943/build/pdf.worker.min.js";
+
+export default({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonContent, IonPage },
+
+  data(){
+    return{
+      url : 'https://firebasestorage.googleapis.com/v0/b/cynohub-v3.appspot.com/o/PDFUniversityNotes%2FFirst%20Year%2FJNTUH%2FEngineering%20Physics%2FOne%20Engineering%20Physics.pdf?alt=media&token=3b59970c-89b7-4edb-a51c-113d15d62627',
+      number_of_pages_to_show : 5,
+      scale : 0.5
+    }
+  },
+
+  async mounted(){
+
+      let container = document.getElementById("pageContainer");
+
+
+      this.pdfViewer = new PDFViewer({
+        container: container
+      });
+
+
+      let loadingTask = pdfjsLib.getDocument(this.url);
+
+      let pdf = await loadingTask.promise;
+
+
+      this.pdfViewer.setDocument(pdf, this.number_of_pages_to_show);
+
+
+      this.pdfViewer._setScale(this.scale)
+
+
+  }
 });
 </script>
